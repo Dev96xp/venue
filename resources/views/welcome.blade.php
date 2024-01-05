75511b56f62daf1e2375dbdd62a0940298355aff
Dev96xp <dip.seniors@gmail.com>
2024-01-05 12:40:03 -0600

    mi segundo comentario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
 
             <div class="w-full md:w-3/4 lg:w-3/4">
                 {{-- APLICANDO UNA CONDICIONAL PARA CLASES --}}
-                <p class="text-black text-7xl md:text-8xl lg:text-8xl py-6" style="font-family: Sche">THE PALACE HALL</p>
+                <p class="text-black text-6xl md:text-8xl lg:text-8xl py-6" style="font-family: Sche">THE PALACE HALL</p>
                 <p class="text-white text-2xl md:text-5xl lg:text-5xl font-bold" style="font-family: Montserrat">
                     Experiance and aptitude of our team
                 </p>
