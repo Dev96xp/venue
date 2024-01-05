551a7d1c60243242b0a6fde9063a3cc1b0f026f4
Dev96xp <dip.seniors@gmail.com>
2024-01-05 11:47:06 -0600

    mi segundo comentario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
             style="background-image: url('{{ asset('img/home/pexels-matheus-bertelli-17023014.jpg') }}'); background-position:center">
 
 
-            <div class="w-full text-6xl md:w-3/4 md:text-8xl lg:w-3/4 lg:text-8xl">
+            <div class="w-full text-7xl md:w-3/4 md:text-8xl lg:w-3/4 lg:text-8xl">
                 <h1 class="text-black py-6" style="font-family: Sche">THE PALACE HALL</h1>
                 <h1 class="text-white font-bold" style="font-family: Montserrat">
                     Experiance and aptitude of our team
