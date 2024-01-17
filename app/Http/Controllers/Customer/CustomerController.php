f37d56c51b48c2c1c98f448e5cb6726c47195a4e
Dev96xp <dip.seniors@gmail.com>
2024-01-16 19:24:18 -0600

    add invitation

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class CustomerController extends Controller
 {
     public function index(){
-        return view('Customer.index');
+        return view('customer.index');
     }
 }
