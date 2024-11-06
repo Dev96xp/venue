a454c30fb5183bd237b6731d061cd1cc4038168f
Dev96xp <dip.seniors@gmail.com>
2024-11-05 21:34:38 -0600

    todo

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -14,6 +14,33 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
 
+        //1
+        Permission::create(['name' => 'Crear cursos',]);
+        //2
+        Permission::create(['name' => 'Leer cursos',]);
+        //3
+        Permission::create(['name' => 'Actualizar cursos',]);
+        //4
+        Permission::create(['name' => 'Eliminar cursos',]);
+        //5
+        Permission::create(['name' => 'Ver dashboard',]);
+
+        //6 - roles
+        Permission::create(['name' => 'Crear role',]);
+        //7
+        Permission::create(['name' => 'Listar role',]);
+        //8
+        Permission::create(['name' => 'Editar role',]);
+        //9
+        Permission::create(['name' => 'Eliminar role',]);
+
+        //10 - users
+        Permission::create(['name' => 'Leer usuarios',]);
+        //11
+        Permission::create(['name' => 'Editar usuarios',]);
+        //12
+        Permission::create(['name' => 'Customer',]);
+
         //13
         Permission::create(['name' => 'Trabajar en Production',]);
         //14
