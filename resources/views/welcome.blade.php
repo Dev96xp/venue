6b94230c0f934e06b67def18ffa4c7046dbaa0b4
Dev96xp <dip.seniors@gmail.com>
2024-12-05 13:59:33 -0600

    css homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -282,7 +282,7 @@
             <div class="col-span-2 ml-4">
 
                 <div class="ml-24 mt-16">
-                    <h3 class=""><span class="text-xl ">{{ $business->name }}</span></h3>
+                    <h1 class=""><span class="text-xl ">{{ $business->name }}</span></h1>
                     <p class="">{{ $business->address . ' ' . $business->city . ' ' . $business->zip }}</p>
                 </div>
 
