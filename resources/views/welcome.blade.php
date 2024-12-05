cda0e598b654893d80987ac84f65c86b9023b1da
Dev96xp <dip.seniors@gmail.com>
2024-12-05 13:44:36 -0600

    css homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -149,7 +149,7 @@
         </h1>
 
         <div
-            class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+            class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
             {{-- ARTICULO 1 --}}
             <article>
                 <figure>
