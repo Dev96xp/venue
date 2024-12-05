0d3490115ce3ab64deb876e12e578ad2a556d632
Dev96xp <dip.seniors@gmail.com>
2024-12-05 14:47:25 -0600

    css homepage

--- app/Livewire/Admin/User/UsersIndex.php
+++ app/Livewire/Admin/User/UsersIndex.php
@@ -47,7 +47,7 @@ class UsersIndex extends Component
         $users = User::where('store_id', $this->store_id)
             ->where('name', 'LIKE', '%' . $this->search . '%')
             ->orderBy($this->sort, $this->direction)
-            ->paginate(5);
+            ->paginate(16);
 
         $stores = Store::all();
 
